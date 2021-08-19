2468fd834ccbf654164fb0705828cf2f24c59541
Dmitriy Bredun <bredun.dmitriy@gmail.com>
2021-08-19 22:10:26 +0300

    Project initialization
    Created projects
    - Lessons
    - HomeworkOleg
    - HomeworkMargaret
    - HomeworkKarina

---
+++ LearnCSH/HomeworkKarina/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeworkKarina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
