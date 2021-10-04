712be05da7c80f2580e4e9b59d20f469c84f3731
Margo <89246277+margo-solo@users.noreply.github.com>
2021-10-04 10:28:48 +0300

    Implemented CT (#11)
    * Implemented CT
    
    * Added MathTool
    
    Co-authored-by: Dmitriy Bredun <bredun.dmitriy@gmail.com>

---
+++ LearnCSH/HomeworkMargaret/Tools/CT.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static System.Console;
+
+namespace HomeworkMargaret.Tools
+{
+    static class CT
+    {
+        public static void Print(int value, string msg = "Result", bool isLine = true)
+        {
+            Write($"{msg} = {value}");
+            if (isLine)
+            {
+                WriteLine();
+            }
+        }
+
+        public static void Print(double value, string msg = "Result", bool isLine = true)
+        {
+            Write($"{msg} = {value}");
+
+            if (isLine)
+            {
+                WriteLine();
+            }
+        }
+        public static void Print(string msg, bool isLine = true)
+        {
+            Write(msg);
+
+            if (isLine)
+            {
+                WriteLine();
+            }
+        }
+
+        public static int EnterInt(string msg)
+        {
+            Write("Enter " + msg + " please: ");
+            return Convert.ToInt32(ReadLine());
+        }
+
+        public static double EnterDouble(string msg)
+        {
+            Write("Enter " + msg + " please: ");
+            return Convert.ToInt32(ReadLine());
+        }
+
+        public static string EnterString(string msg)
+        {
+            Write("Enter " + msg + ", please: ");
+            return ReadLine();
+        }
+        public static void ErrorMsg(string msg)
+        {
+            ForegroundColor = ConsoleColor.Red;
+
+            WriteLine();
+            WriteLine("!ERROR: " + msg.ToUpper());
+            WriteLine();
+
+            ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ LearnCSH/HomeworkMargaret/Tools/MT.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkMargaret.Tools
+{
+    class MT
+    {
+        public static double Sum(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+
+        public static double Subst(double v1, double v2)
+        {
+            return v1 - v2;
+        }
+
+        public static double Mult(double v1, double v2)
+        {
+            return v1 * v2;
+        }
+
+        public static double Divish(double v1, double v2)
+        {
+            if (v2 == 0)
+            {
+                CT.ErrorMsg("wrong operation");
+                return 0;
+            }
+
+            return v1 / v2;
+        }
+
+        public static double Pow(double v, double n)
+        {
+           return Math.Pow(v, n);
+        }
+
+        public static double Sqrt(double v)
+        {
+            return Math.Sqrt(v);
+        }
+
+        public static double Abs(double v)
+        {
+            return Math.Abs(v);
+        }
+    }
+}
