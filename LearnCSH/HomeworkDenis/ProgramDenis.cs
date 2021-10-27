576e03390062139353caa2b05990fc48c92873a0
Denis <48459415+DenisBredun@users.noreply.github.com>
2021-10-27 17:11:34 +0300

    Merge Denis Project

---
+++ LearnCSH/HomeworkDenis/ProgramDenis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HomeworkDenis.General;
+
+namespace HomeworkDenis
+{
+    class ProgramDenis
+    {
+        static void Main(string[] args)
+        {
+            //Homework2_Operations.Start();
+            //Homework3_Cycles.Start();
+            Homework4_SimpleArrays.Start();
+        }
+    }
+}
