a5b73af75cbce1da825a14798e7d7761ade9c883
Margo <89246277+margo-solo@users.noreply.github.com>
2021-11-05 15:53:39 +0200

    Merge: HW5 - DoubleArray Margaret
    * Homework5_DoubleArrays
    * Added last task

--- LearnCSH/HomeworkMargaret/ProgramMargaret.cs
+++ LearnCSH/HomeworkMargaret/ProgramMargaret.cs
@@ -30,7 +30,8 @@ namespace HomeworkMargaret
             // Homework3_Cycles.Start();
             // Homework3_PartTwo.Start();
             // Homework3_PartThree.Start();
-            Homework4_SimpleArrays.Start();
+            // Homework4_SimpleArrays.Start();
+            Homework5_DoubleArrays.Start();
         }
     }
 }
