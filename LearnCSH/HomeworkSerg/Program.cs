e41f8d1991cb361825a157afcfa0d0e7cbd40935
Metrorul <93949306+Metrorul@users.noreply.github.com>
2021-11-14 10:46:29 +0200

    Merge Hw1_Serg

---
+++ LearnCSH/HomeworkSerg/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using HomeworkSerg.General;
+
+namespace HomeworkSerg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Homework1_Variables.Start();
+        }
+    }
+}
