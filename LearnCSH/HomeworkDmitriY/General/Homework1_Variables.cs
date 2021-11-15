75788b7fc739f724417f7a0924fe290198e36a58
ZloyDer <zloyder@gmail.com>
2021-11-15 23:42:53 +0200

    Homework1_Start

---
+++ LearnCSH/HomeworkDmitriY/General/Homework1_Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDmitriy.General
+{
+    internal class Homework1_Variables
+    {
+        public static void Startuem()
+        {
+            Console.WriteLine("Эй путник...");
+        }
+    }
+}
