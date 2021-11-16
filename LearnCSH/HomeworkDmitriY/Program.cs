22a81da305d7fd390b485c5446124d40bd1f61ff
Dmitriy Bredun <bredun.dmitriy@gmail.com>
2021-11-16 12:08:49 +0300

    Merged HW1 - Dmitriy
    * Homework1_Start
    * HW1_Completed
    * Update homework1

---
+++ LearnCSH/HomeworkDmitriY/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using HomeworkDmitriy.General;
+
+namespace HomeworkDmitriy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Homework1_Variables.Startuem();
+        }
+    }
+}
