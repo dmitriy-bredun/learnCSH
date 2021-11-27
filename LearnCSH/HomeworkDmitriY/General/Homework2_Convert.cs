d2403773d84ff09e3b4dd9c6baa2ae73e669457d
ZloyDer <zloyder@gmail.com>
2021-11-27 04:05:58 +0200

    update_2

---
+++ LearnCSH/HomeworkDmitriY/General/Homework2_Convert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDmitriy.General
+{
+    internal class Homework2_Convert
+    {
+        public static void Start0()
+        {
+            // int intVal = 1234;
+            // long longVal = (long)intVal;
+
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            int str1 = Convert.ToInt32(name1);
+            int str2 = Convert.ToInt32(name2);
+            Console.WriteLine($"name1 = {str1}");
+            Console.WriteLine($"name2 = {str2}");
+            Console.WriteLine($"name1 + name2");
+            Console.WriteLine($"name1 + name2 = {str1 + str2}");
+        }
+    }
+}
--- LearnCSH/HomeworkDmitriY/Program.cs
+++ LearnCSH/HomeworkDmitriY/Program.cs
@@ -8,9 +8,13 @@ namespace HomeworkDmitriy
     {
         static void Main(string[] args)
         {
+            // Homework0_Test.Test();
             // Homework1_Variables.Startuem();
+            // Homework2_Convert.Start0();
             // Homework3_Operations.Start1();
-            Homework3_Operations.Start2();
+            // Homework3_Operations.Start2();
+            // Homework3_Operations.Start3();
+            // Homework3_Operations.Start4();
         }
     }
 }
