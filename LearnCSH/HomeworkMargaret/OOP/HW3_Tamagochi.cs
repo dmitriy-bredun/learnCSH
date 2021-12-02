08ad6cb33abe784a7a7efba127fc9c3e6982aa0d
Margo <sologubmargaret@gmail.com>
2021-12-02 16:27:21 +0200

    HW3_Start

---
+++ LearnCSH/HomeworkMargaret/OOP/HW3_Tamagochi.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+using HomeworkMargaret.Tools;
+
+namespace HomeworkMargaret.OOP
+{
+    class Cat
+    {
+        public string name;
+        public double levelOfHunger;
+        public double levelOfHappiness;
+        public double levelOfTiredness;
+        public string toy;
+        public string faveToy;
+        public string food;
+        public string faveFood;
+
+        public Cat()
+        {
+            
+        }
+
+        enum Toys
+        {
+            Ball,
+            Mouse,
+            CandyWrapper,
+            Ribbon,
+            Laser,
+            Box
+        }
+
+        enum Food
+        {
+            Feed,
+            Jelly,
+            Chicken,
+            Tuna,
+            Salmon,
+            Grass
+        }
+    }
+
+    class Tamagotchi
+    {
+        Cat cat = new Cat();       
+        public static void Start()
+        {
+            
+        }
+        
+        public static void PlayWithaToy()
+        {
+              
+        }
+
+        public static void Eat()
+        {
+            int faveToy;
+            // if(faveFood)
+            {
+              
+            }
+            else
+            {
+
+            }
+
+            //if (levelOfTiredness > 60)
+            {
+                CT.Print("Thank you for the game! I've had a lot of fun! But now I need to have some rest.");
+            }
+        }
+
+        public static void Sleep()
+        {
+
+        }
+
+        public static void ShowCurrentValues()
+        {
+            
+        }
+    }
+}
