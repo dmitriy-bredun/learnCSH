5bfce94d022358f5aac945dc9308283ffb694c71
Margo <sologubmargaret@gmail.com>
2022-01-13 10:09:08 +0200

    Not finished yet

--- LearnCSH/HomeworkMargaret/ProgramMargaret.cs
+++ LearnCSH/HomeworkMargaret/ProgramMargaret.cs
@@ -41,8 +41,9 @@ namespace HomeworkMargaret
             // Homework1_Classes.Start();
             //Homework1_PandoraBadassesRegistration.Start();
             //HW3_Tamagochi.Start();
-            Task.Start();
+            //Task.Start();
             //Game.Start();
+            Lights.Start();
         }
     }
 }
