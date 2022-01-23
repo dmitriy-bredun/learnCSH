b5617f7ecbcdd2f392489559810bf1d111635c76
Metrorul <metroruls@gmail.com>
2022-01-23 10:38:55 +0200

    ToolArray

---
+++ LearnCSH/HomeworkSerg/ToolArray/AT.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace HomeworkSerg.Tools
+{
+    internal class AT
+    {
+        public static int[] CreateSimplArray(int size)
+        {
+            if (size <= 0)
+            {
+                WriteLine("Масив с таким размером создать невозможно.");
+                return null;
+            }
+            int[] newArray = new int[size];
+            return newArray;
+        }
+        
+        public static void FillingManuall(int[] Array)
+        {
+            for (int indx = 0; indx < Array.Length; indx++)
+            {
+                Write($"Число для сохранения в ячейку [{indx}]: ");
+                Array[indx] = Convert.ToInt32(ReadLine());
+            }
+            WriteLine("Масив заполнен.");
+        }
+        public static void FillingRandom(int[] Array, int min, int max)
+        {
+            Random rand = new Random();
+            for (int indx = 0; indx < Array.Length; indx++)
+            {
+                WriteLine($"Случайное число: {Array[indx] = rand.Next(min, max)}");
+            }
+            WriteLine("Масив заполнен.");
+        }
+    }
+}
--- LearnCSH/HomeworkSerg/Tools/MT.cs
+++ LearnCSH/HomeworkSerg/Tools/MT.cs
@@ -27,13 +27,5 @@ namespace HomeworkSerg.Tools
             Write("W = ");
             return (4 * Q) / (3.14 * Math.Pow(D, 2));
         }
-        public static double Filling(int[] Array)
-        {
-            for (int indx = 0; indx < Array.Length; indx++)
-            {
-                WriteLine($"Число для сохранения в ячейку [{indx}]: ");
-                return Array[indx] = Convert.ToInt32(ReadLine());
-            }
-        }
     }
 }
