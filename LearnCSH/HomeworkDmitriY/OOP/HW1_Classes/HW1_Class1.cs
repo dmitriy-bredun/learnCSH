438b426efffee152420d409d974a6a33cc92ef7d
ZloyDer <zloyder@gmail.com>
2022-06-15 17:43:59 +0300

    HW1_Classes_Dmitriy

---
+++ LearnCSH/HomeworkDmitriY/OOP/HW1_Classes/HW1_Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using HomeworkDmitriy.Tools;
+using static System.Console;
+using static System.Convert;
+
+namespace HomeworkDmitriy.OOP.HW1_Classes
+{
+    internal class HW1_Class1
+    {
+        public static void Start()
+        {
+            CT.Print("Введите размер массива: ");
+            double frequency = ToInt32(ReadLine());
+
+            Processor processor = new Processor (4.9, "AMD", 16, "white", "sweet", 64);
+
+            
+
+        }
+    }
+}
---
+++ LearnCSH/HomeworkDmitriY/OOP/HW1_Classes/Processor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDmitriy.OOP.HW1_Classes
+{
+    class Processor
+    {
+        public double frequency;
+        public string companyName;
+        public int countOfCores;
+        public int cache;
+
+        public Processor(double frequency, string companyN, int cores, int cache)
+        {
+            this.frequency = frequency;
+            companyName = companyN;
+            countOfCores = cores;
+            this.cache = cache;
+        }
+    }
+}
