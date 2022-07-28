ee0a3bdcb95b8d79c1125a5139acd0094a2f0b88
Margo <sologubmargaret@gmail.com>
2022-07-28 21:26:56 +0300

    Calculator is done

---
+++ LearnCSH/HomeworkMargaret/Tools/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkMargaret.Tools
+{
+    enum ArithmeticOperation
+    {
+        Add,
+        Substract,
+        Multiply,
+        Divide
+    }
+    class Calculator
+    {
+        public static double ArithOperation(int numb, int numb2, ArithmeticOperation operation)
+        {
+            int sum = 0;
+            switch (operation)
+            {
+                case ArithmeticOperation.Add:
+                    sum = numb + numb2;
+                    break;
+
+                case ArithmeticOperation.Substract:
+                    sum = numb - numb2;
+                    break;
+
+                case ArithmeticOperation.Multiply:
+                    sum = numb * numb2;
+                    break;
+
+                case ArithmeticOperation.Divide:
+                    if (numb2 == 0)
+                    {
+                        CT.ErrorMsg("wrong operation");
+                        return 0;
+                    }
+
+                    sum = numb / numb2;
+                    break;
+
+                default:
+                    break;
+            }
+            return sum;
+        }
+    }
+}
