4dd5d6de88899512414270183d85efa49069c495
Margo <89246277+margo-solo@users.noreply.github.com>
2022-08-02 23:18:41 +0300

    Merged: HW Calculator - Margaret
    * Calculator is done

---
+++ LearnCSH/HomeworkMargaret/Tools/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkMargaret.Tools
+{
+    enum ArithmeticOperation
+    {
+        Add,
+        Substract,
+        Multiply,
+        Divide
+    }
+    class Calculator
+    {
+        public static double ArithOperation(int numb, int numb2, ArithmeticOperation operation)
+        {
+            int mathOperation = operation switch
+            {
+                ArithmeticOperation.Add => numb + numb2,
+                ArithmeticOperation.Substract => numb - numb2,
+                ArithmeticOperation.Multiply => numb * numb2,
+                ArithmeticOperation.Divide => numb / numb2,
+                _ => 0,
+            };
+
+            return mathOperation;
+        }
+    }
+}
