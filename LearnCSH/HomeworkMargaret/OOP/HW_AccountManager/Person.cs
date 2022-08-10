7f6795483bd2e1ca3581662544b7dfc566f4ed7a
Margo <sologubmargaret@gmail.com>
2022-08-10 12:18:36 +0300

    started hw. created classes and methods lol

---
+++ LearnCSH/HomeworkMargaret/OOP/HW_AccountManager/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkMargaret.OOP.HW_AccountManager
+{
+    class Person
+    {
+        private string Name;
+        private int Age;
+        Profession Prof;
+        Person MyFather;
+        Person MyMother;
+        List<Person> friends;
+
+        public Person(string name, int age, Profession prof)
+        {
+            name = Name;
+            age = Age;
+            prof = Prof;
+        }
+    }
+}
