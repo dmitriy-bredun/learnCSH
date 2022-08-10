690e8d1a5476447b332dd2409d1d048bdcf59c9e
Margo <sologubmargaret@gmail.com>
2022-08-10 12:18:36 +0300

    started hw. created classes and methods lol

---
+++ LearnCSH/HomeworkMargaret/OOP/HW_AccountManager/HW_AccountManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static HomeworkMargaret.Tools.CT;
+
+namespace HomeworkMargaret.OOP.HW_AccountManager
+{
+    class HW_AccountManager
+    {
+        List<Person> friends = new List<Person>();
+        List<Person> people = new List<Person>();
+        public static void Start()
+        {
+            int userChoice;
+            do
+            {
+                Space();
+                userChoice = Menu(
+                    " Add new profile ",
+                    " Change profession ",
+                    " Choose a father ",
+                    " Choose a mother ",
+                    " Add a friend ",
+                    " Remove a friend ");
+
+                switch (userChoice)
+                {
+                    case 1:
+                        AddNewProfile();
+                        break;
+
+                    case 2:
+                        ChangeProfession();
+                        break;
+
+                    case 3:
+                        ChooseAFather();
+                        break;
+
+                    case 4:
+                        ChooseAMother();
+                        break;
+
+                    case 5:
+                        AddAFriend();
+                        break;
+
+                    case 6:
+                        RemoveAFriend();
+                        break;
+
+                    default:
+                        Print("Oops, smth went wrong");
+                        break;
+                }
+
+            } while (userChoice != 0);
+        }
+
+        public static void AddNewProfile()
+        {
+            string name = EnterString("Name: ");
+            int age = EnterInt("Age: ");
+            Profession prof = (Profession)EnterInt("Choose a profession: ");
+            Person person = new Person(name, age, prof);
+            people.Add(1);
+        }
+
+        public static void ChangeProfession()
+        {
+            
+        }
+
+        public static void ChooseAFather()
+        {
+
+        }
+
+        public static void ChooseAMother()
+        {
+
+        }
+
+        public static void AddAFriend()
+        {
+
+        }
+
+        public static void RemoveAFriend()
+        {
+
+        }
+    }
+}
---
+++ LearnCSH/HomeworkMargaret/OOP/HW_AccountManager/Profession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkMargaret.OOP.HW_AccountManager
+{
+    enum Profession
+    {
+        Teacher = 1,
+        Programmer,
+        Driver,
+        Manager,
+        Trainer,
+        Freelancer,
+        Unemployed
+    }
+
+}
